fbc90e75604546101429f8363ca15d301448129b
Maxwell Baird <bairdm71@gmail.com>
2020-03-27 09:58:19 -0600

    created average age for the professor model

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -4,4 +4,13 @@ class Professor <ApplicationRecord
   has_many :students, through: :professor_students
 
   validates_presence_of :name, :age, :specialty
+
+  def average_age_of_students
+    sum = 0.0
+    students.each do |student|
+      sum += student.age
+    end
+    average = sum / students.length
+    average.round(2)
+  end
 end
--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -9,17 +9,23 @@ RSpec.describe "As a visitor", type: :feature do
     student_3 = professor.students.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
     visit "/professors/#{professor.id}"
 
-
-    expect(page).to have_content(student_1.name)
-    expect(page).to have_no_content(student_1.age)
-    expect(page).to have_no_content(student_1.house)
-    expect(page).to have_content(student_2.name)
-    expect(page).to have_no_content(student_2.age)
-    expect(page).to have_no_content(student_2.house)
-    expect(page).to have_content(student_3.name)
-    expect(page).to have_no_content(student_3.age)
-    expect(page).to have_no_content(student_3.house)
+    within("#student-#{student_1.id}") do
+      expect(page).to have_content(student_1.name)
+      expect(page).to have_no_content(student_1.age)
+      expect(page).to have_no_content(student_1.house)
+    end
+    within("#student-#{student_2.id}") do
+      expect(page).to have_content(student_2.name)
+      expect(page).to have_no_content(student_2.age)
+      expect(page).to have_no_content(student_2.house)
+    end
+    within("#student-#{student_3.id}") do
+      expect(page).to have_content(student_3.name)
+      expect(page).to have_no_content(student_3.age)
+      expect(page).to have_no_content(student_3.house)
+    end
   end
+
   it "I can see a professor's students average age" do
 
     professor = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")

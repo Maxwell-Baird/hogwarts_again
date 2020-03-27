5fbcbec644deb407c78270d0e906181f1e4d71e4
Maxwell Baird <bairdm71@gmail.com>
2020-03-27 09:31:59 -0600

    created the professor/:id test

---
+++ spec/features/professors/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  it "I can see a professor at '/professors/:id'" do
+
+    professor = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    student_1 = professor.students.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    student_2 = professor.students.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    student_3 = professor.students.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+    visit "/professors/:id"
+
+
+    expect(page).to have_content(student_1.name)
+    expect(page).to have_no_content(student_1.age)
+    expect(page).to have_no_content(student_1.house)
+    expect(page).to have_content(student_2.name)
+    expect(page).to have_no_content(student_2.age)
+    expect(page).to have_no_content(student_2.house)
+    expect(page).to have_content(student_3.name)
+    expect(page).to have_no_content(student_3.age)
+    expect(page).to have_no_content(student_3.house)
+  end
+end

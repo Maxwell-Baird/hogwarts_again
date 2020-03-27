ad1e6c62fde1af81e6d2f3f9076e5917317232ed
Maxwell Baird <bairdm71@gmail.com>
2020-03-27 09:49:29 -0600

    created a test to check average age of students

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor", type: :feature do
-  it "I can see a professor at '/professors/:id'" do
+  it "I can see a professor's students at '/professors/:id'" do
 
     professor = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
     student_1 = professor.students.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
@@ -19,5 +19,17 @@ RSpec.describe "As a visitor", type: :feature do
     expect(page).to have_content(student_3.name)
     expect(page).to have_no_content(student_3.age)
     expect(page).to have_no_content(student_3.house)
+  end
+  it "I can see a professor's students average age" do
+
+    professor = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    student_1 = professor.students.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    student_2 = professor.students.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    student_3 = professor.students.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+    visit "/professors/#{professor.id}"
+
+
+    expect(page).to have_content("Average Age: 11.33")
+
   end
 end
